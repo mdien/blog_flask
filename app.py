714d50105f0496de342fb1c08319a64db8869146
mdien <m3g3nz2@gmail.com>
2020-05-02 11:03:38 +0700

    Simple Blog App - Frontend first commit

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+Bootstrap(app)
+
+@app.route('/', methods=['GET','POST'])
+def index():
+    return render_template('index.html')
+
+@app.route('/about/')
+def about():
+    return render_template('about.html')
+
+@app.route('/blogs/<int:id>/')
+def blogs(id):
+    return render_template('blogs.html', blog_id=id)
+
+@app.route('/register/', methods=['GET','POST'])
+def register():
+    return render_template('register.html')
+
+@app.route('/login/', methods=['GET','POST'])
+def login():
+    return render_template('login.html')
+
+@app.route('/write-blog/', methods=['GET','POST'])
+def write_blog():
+    return render_template('write-blog.html')
+
+@app.route('/my-blogs/')
+def my_blogs():
+    return render_template('my-blogs.html')
+
+@app.route('/edit-blog/', methods=['GET','POST'])
+def edit_blog():
+    return render_template('edit-blog.html')
+
+@app.route('/delete-blog/<int:id>/', methods=['POST'])
+def delete_blogs(id):
+    return 'success'
+
+@app.route('/logout/')
+def logout():
+    return render_template('logout.html')
+
+@app.route()
+
+if __name__ == '__main__':
+    app.run()
